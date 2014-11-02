6515616cc1b72e46a80799f1712f8253ee549683
Paul P <paulp@hotmail.sk>
2014-11-02 16:45:26 +0100

    simspark constants

---
+++ Jim/src/sk/fiit/jim/agent/moves/ik/SimsparkConstants.java
@@ -0,0 +1,35 @@
+package sk.fiit.jim.agent.moves.ik;
+
+public class SimsparkConstants
+{
+    // milimeters
+    static final double NECK_OFFSET_Z = 90;
+
+    static final double SHOULDER_OFFSET_Y = 98;
+
+    static final double ELBOW_OFFSET_Y = 0;
+
+    static final double UPPER_ARM_LENGTH = 90;
+
+    static final double LOWER_ARM_LENGTH = 105;
+
+    static final double SHOULDER_OFFSET_Z = 75;
+
+    static final double HAND_OFFSET_X = 0;
+
+    static final double HIP_OFFSET_Z = 115;
+
+    static final double HIP_OFFSET_Y = 55;
+
+    static final double THIGH_LENGHT = 120;
+
+    static final double TIBIA_LENGHT = 100;
+
+    static final double FOOT_HEIGHT = 50;
+
+    static final double HAND_OFFSET_Z = 9;
+    
+    private SimsparkConstants()
+    {
+    }
+}
