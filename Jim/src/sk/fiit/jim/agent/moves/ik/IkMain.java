a26791e4f412de9b1995185bee5bb9140432d39d
Paul P <paulp@hotmail.sk>
2014-11-08 18:01:46 +0100

    progress for IK left leg

--- Jim/src/sk/fiit/jim/agent/moves/ik/IkMain.java
+++ Jim/src/sk/fiit/jim/agent/moves/ik/IkMain.java
@@ -8,67 +8,122 @@ public class IkMain
 {
     private static final Point3D DEF_LEFT_ARM = new Point3D(195, 98, 75); // predpazena
     
+    private static final Point3D DEF_RIGHT_ARM = new Point3D(195, -98, 75); // predpazena
+    
     private static final Point3D DEF_LEFT_ARM_TORSO = new Point3D(190, 100, 75);
+    
+    private static final Point3D DEF_LEFT_LEG = new Point3D(0.0, 55.0, -385.0);
+    
+    private static final Point3D DEF_RIGHT_LEG = new Point3D(0.0, -55.0, -385.0);
     
     public static void main(String[] args)
     {
         Angle angle = new Angle(0, 0, 0);
         //Point3D end = new Point3D(0, 0, 0);
-        Point3D end = DEF_LEFT_ARM;
-        LeftArmIk la = new LeftArmIk(end, angle);
-        System.out.println("theta4: " + la.getTheta4());
-        System.out.println("theta4: " + Math.toDegrees(la.getTheta4()) +"°");
-        System.out.println("theta2: " + la.getTheta2());
-        System.out.println("theta2: " + Math.toDegrees(la.getTheta2()) +"°");
-        System.out.println("theta3_1: " + la.getTheta3_1());
-        System.out.println("theta3_1: " + Math.toDegrees(la.getTheta3_1()) +"°");
-        System.out.println("theta1: " + la.getTheta1());
-        System.out.println("theta1: " + Math.toDegrees(la.getTheta1()) +"°");
-        System.out.println("theta3_2: " + la.getTheta3_2());
-        System.out.println("theta3_2: " + Math.toDegrees(la.getTheta3_2()) +"°");
-        System.out.println("theta1: " + la.getTheta1());
-        System.out.println("theta1: " + Math.toDegrees(la.getTheta1()) +"°");
-        System.out.println("theta2_b: " + la.getTheta2_b());
-        System.out.println("theta2_b: " + Math.toDegrees(la.getTheta2_b()) +"°");
-        
-        long time = System.nanoTime();
+        Point3D endLeftArm = DEF_LEFT_ARM;
+        LeftArmIk la = new LeftArmIk(endLeftArm, angle);
         System.out.println(la.getResult());
-        long diff = System.nanoTime() - time;
-        System.out.println(diff / 1000000000.0);
         
-        double[][] Abase = MatrixOperations.createTranslation(0, SHOULDER_OFFSET_Y + ELBOW_OFFSET_Y, SHOULDER_OFFSET_Z);
-        double[][] T01 = MatrixOperations.createDHTransformation(0, - PI/2, 0, 0); // a, alpha, d, theta
-        double[][] T12 = MatrixOperations.createDHTransformation(0, PI/2, 0, - PI/2);
-        double[][] T23 = MatrixOperations.createDHTransformation(0, - PI/2, UPPER_ARM_LENGTH, 0);
-        double[][] T34 = MatrixOperations.createDHTransformation(0, PI/2, 0, 0);
-        double[][] Rz = MatrixOperations.createRotationZ(PI/2);
-        double[][] Aend = MatrixOperations.createTranslation(HAND_OFFSET_X + LOWER_ARM_LENGTH, 0, 0);
+        Point3D endRightArm = DEF_RIGHT_ARM;
+        RightArmIk ra = new RightArmIk(endRightArm, angle);
+        System.out.println(ra.getResult());
         
-        double[][] T = MatrixOperations.mult(Abase, T01);
-        T = MatrixOperations.mult(T, T12);
-        T = MatrixOperations.mult(T, T23);
-        T = MatrixOperations.mult(T, T34);
-        T = MatrixOperations.mult(T, Rz);
-        T = MatrixOperations.mult(T, Aend);
+        Point3D endLeftLeg = DEF_LEFT_LEG;
+        LeftLegIk le = new LeftLegIk(endLeftLeg, angle);
+        System.out.println(le.getResult());
+       
+        // left arm forward
+        double[][] AbaseLeftArm = MatrixOperations.createTranslation(0, SHOULDER_OFFSET_Y + ELBOW_OFFSET_Y, SHOULDER_OFFSET_Z);
+        double[][] T01LeftArm = MatrixOperations.createDHTransformation(0, - PI/2, 0, 0); // a, alpha, d, theta
+        double[][] T12LeftArm = MatrixOperations.createDHTransformation(0, PI/2, 0, - PI/2);
+        double[][] T23LeftArm = MatrixOperations.createDHTransformation(0, - PI/2, UPPER_ARM_LENGTH, 0);
+        double[][] T34LeftArm = MatrixOperations.createDHTransformation(0, PI/2, 0, 0);
+        double[][] RzLeftArm = MatrixOperations.createRotationZ(PI/2);
+        double[][] AendLeftArm = MatrixOperations.createTranslation(HAND_OFFSET_X + LOWER_ARM_LENGTH, 0, 0);
         
-        MatrixOperations.print(T);
-        System.out.println(new ForwardKinematicResult(T));
+        double[][] TLeftArm = MatrixOperations.mult(AbaseLeftArm, T01LeftArm);
+        TLeftArm = MatrixOperations.mult(TLeftArm, T12LeftArm);
+        TLeftArm = MatrixOperations.mult(TLeftArm, T23LeftArm);
+        TLeftArm = MatrixOperations.mult(TLeftArm, T34LeftArm);
+        TLeftArm = MatrixOperations.mult(TLeftArm, RzLeftArm);
+        TLeftArm = MatrixOperations.mult(TLeftArm, AendLeftArm);
         
-        la = new LeftArmIk(T);
-        System.out.println("theta4: " + la.getTheta4());
-        System.out.println("theta4: " + Math.toDegrees(la.getTheta4()) +"°");
-        System.out.println("theta2: " + la.getTheta2());
-        System.out.println("theta2: " + Math.toDegrees(la.getTheta2()) +"°");
-        System.out.println("theta3_1: " + la.getTheta3_1());
-        System.out.println("theta3_1: " + Math.toDegrees(la.getTheta3_1()) +"°");
-        System.out.println("theta1: " + la.getTheta1());
-        System.out.println("theta1: " + Math.toDegrees(la.getTheta1()) +"°");
-        System.out.println("theta3_2: " + la.getTheta3_2());
-        System.out.println("theta3_2: " + Math.toDegrees(la.getTheta3_2()) +"°");
-        System.out.println("theta1: " + la.getTheta1());
-        System.out.println("theta1: " + Math.toDegrees(la.getTheta1()) +"°");
-        System.out.println("theta2_b: " + la.getTheta2_b());
-        System.out.println("theta2_b: " + Math.toDegrees(la.getTheta2_b()) +"°");
+        System.out.println("left arm");
+        MatrixOperations.print(TLeftArm);
+        System.out.println(new ForwardKinematicResult(TLeftArm));
         
+        // Right arm forward
+        double[][] AbaseRightArm = MatrixOperations.createTranslation(0, -SHOULDER_OFFSET_Y - ELBOW_OFFSET_Y, SHOULDER_OFFSET_Z);
+        double[][] T01RightArm = MatrixOperations.createDHTransformation(0, - PI/2, 0, 0); // a, alpha, d, theta
+        double[][] T12RightArm = MatrixOperations.createDHTransformation(0, PI/2, 0, PI/2);
+        double[][] T23RightArm = MatrixOperations.createDHTransformation(0, - PI/2, -UPPER_ARM_LENGTH, 0);
+        double[][] T34RightArm = MatrixOperations.createDHTransformation(0, PI/2, 0, 0);
+        double[][] RzRightArm = MatrixOperations.createRotationZ(PI/2);
+        double[][] AendRightArm = MatrixOperations.createTranslation(-HAND_OFFSET_X - LOWER_ARM_LENGTH, 0, 0);
+        double[][] RzRightArm2 = MatrixOperations.createRotationZ(-PI);
+        
+        double[][] TRightArm = MatrixOperations.mult(AbaseRightArm, T01RightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, T12RightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, T23RightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, T34RightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, RzRightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, AendRightArm);
+        TRightArm = MatrixOperations.mult(TRightArm, RzRightArm2);
+        
+        System.out.println("Right arm");
+        MatrixOperations.print(TRightArm);
+        System.out.println(new ForwardKinematicResult(TRightArm));
+        
+        // left leg forward
+        double[][] AbaseLeftLeg = MatrixOperations.createTranslation(0, HIP_OFFSET_Y, -HIP_OFFSET_Z);
+        double[][] T01LeftLeg = MatrixOperations.createDHTransformation(0, - 3*PI/4, 0, -PI/2); // a, alpha, d, theta
+        double[][] T12LeftLeg = MatrixOperations.createDHTransformation(0, -PI/2, 0, PI/4);
+        double[][] T23LeftLeg = MatrixOperations.createDHTransformation(0, PI/2, 0, 0);
+        double[][] T34LeftLeg = MatrixOperations.createDHTransformation(-THIGH_LENGHT, 0, 0, 0);
+        double[][] T45LeftLeg = MatrixOperations.createDHTransformation(-TIBIA_LENGHT, 0, 0, 0);
+        double[][] T56LeftLeg = MatrixOperations.createDHTransformation(0, -PI/2, 0, 0);
+        double[][] RzLeftLeg = MatrixOperations.createRotationZ(PI);
+        double[][] RyLeftLeg = MatrixOperations.createRotationY(-PI/2);
+        double[][] AendLeftLeg = MatrixOperations.createTranslation(0, 0, -FOOT_HEIGHT);
+        
+        double[][] TLeftLeg = MatrixOperations.mult(AbaseLeftLeg, T01LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, T12LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, T23LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, T34LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, T45LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, T56LeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, RzLeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, RyLeftLeg);
+        TLeftLeg = MatrixOperations.mult(TLeftLeg, AendLeftLeg);
+        
+        System.out.println("left leg");
+        MatrixOperations.print(TLeftLeg);
+        System.out.println(new ForwardKinematicResult(TLeftLeg));
+        
+        // Right leg forward
+        double[][] AbaseRightLeg = MatrixOperations.createTranslation(0, -HIP_OFFSET_Y, -HIP_OFFSET_Z);
+        double[][] T01RightLeg = MatrixOperations.createDHTransformation(0, - PI/4, 0, -PI/2); // a, alpha, d, theta
+        double[][] T12RightLeg = MatrixOperations.createDHTransformation(0, -PI/2, 0, -PI/4);
+        double[][] T23RightLeg = MatrixOperations.createDHTransformation(0, PI/2, 0, 0);
+        double[][] T34RightLeg = MatrixOperations.createDHTransformation(-THIGH_LENGHT, 0, 0, 0);
+        double[][] T45RightLeg = MatrixOperations.createDHTransformation(-TIBIA_LENGHT, 0, 0, 0);
+        double[][] T56RightLeg = MatrixOperations.createDHTransformation(0, -PI/2, 0, 0);
+        double[][] RzRightLeg = MatrixOperations.createRotationZ(PI);
+        double[][] RyRightLeg = MatrixOperations.createRotationY(-PI/2);
+        double[][] AendRightLeg = MatrixOperations.createTranslation(0, 0, -FOOT_HEIGHT);
+        
+        double[][] TRightLeg = MatrixOperations.mult(AbaseRightLeg, T01RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, T12RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, T23RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, T34RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, T45RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, T56RightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, RzRightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, RyRightLeg);
+        TRightLeg = MatrixOperations.mult(TRightLeg, AendRightLeg);
+        
+        System.out.println("Right leg");
+        MatrixOperations.print(TRightLeg);
+        System.out.println(new ForwardKinematicResult(TRightLeg));
     }
 }
--- Jim/src/sk/fiit/jim/agent/moves/ik/LeftLegIk.java
+++ Jim/src/sk/fiit/jim/agent/moves/ik/LeftLegIk.java
@@ -1,12 +1,16 @@
 package sk.fiit.jim.agent.moves.ik;
 
 import static java.lang.Math.*;
+import static sk.fiit.jim.agent.moves.ik.SimsparkConstants.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import sk.fiit.jim.agent.moves.Joint;
 import sk.fiit.robocup.library.geometry.Point3D;
 
 public class LeftLegIk
 {
-    private double[][] T = new double[4][4];
-    
     private double theta1;
 
     private double theta2;
@@ -19,9 +23,20 @@ public class LeftLegIk
     
     private double theta6;
 
-    private double l1; //TODO
+    private double l1 = THIGH_LENGHT; 
 
-    private double l2; // TODO
+    private double l2 = TIBIA_LENGHT;
+    
+    private double sx = 0;
+    
+    private double sy = 0;
+    
+    private double sz = 0; 
+    
+    private double[][] T = new double[4][4];
+    private double[][] T_ = new double[4][4];
+    private double[][] T__ = new double[4][4];
+    private double[][] T___ = new double[4][4];
     
     public LeftLegIk(Point3D end, Angle angle)
     {
@@ -47,23 +62,169 @@ public class LeftLegIk
         T[3][1] = 0;
         T[3][2] = 0;
         T[3][3] = 1;
+        
+        double[][] temp = MatrixOperations.mult(T, MatrixOperations.invert(MatrixOperations.createTranslation(0, 0, -FOOT_HEIGHT)));
+        temp = MatrixOperations.mult(MatrixOperations.invert(MatrixOperations.createTranslation(0, HIP_OFFSET_Y, -HIP_OFFSET_Z)), temp);
+        temp = MatrixOperations.invert(temp);
+        T_ = MatrixOperations.mult(MatrixOperations.createRotationX(PI / 4), temp);
+        T_ = MatrixOperations.invert(T_);
+        
+        // TODO posun vyssie a usetris vypocet
+        double[][] left = MatrixOperations.invert(T_);
+        double[][] T56LeftLeg = MatrixOperations.createDHTransformation(0, -PI/2, 0, 0);
+        double[][] RzLeftLeg = MatrixOperations.createRotationZ(PI);
+        double[][] RyLeftLeg = MatrixOperations.createRotationY(-PI/2);
+        double[][] right = MatrixOperations.mult(T56LeftLeg, RzLeftLeg);
+        right = MatrixOperations.mult(right, RyLeftLeg);
+        right = MatrixOperations.invert(right);
+        T__ = MatrixOperations.mult(left, right);
+        T__ = MatrixOperations.invert(T__);
+        
+        double[][] T34LeftLeg = MatrixOperations.createDHTransformation(-THIGH_LENGHT, 0, 0, 0);
+        double[][] T45LeftLeg = MatrixOperations.createDHTransformation(-TIBIA_LENGHT, 0, 0, 0);
+        double[][] TTemp = MatrixOperations.mult(T34LeftLeg, T45LeftLeg);
+        TTemp = MatrixOperations.invert(TTemp);
+        T___ = MatrixOperations.mult(MatrixOperations.invert(T__), TTemp);
     }
     
-    public double getTheta4()
+    double getTheta4()
     {
-        double T14_ = 0.0; // TODO
-        double T24_ = 0.0; // TODO
-        double T34_ = 0.0; // TODO
-        double d = sqrt((-T14_)*(-T14_) + (-T24_ ) * (-T24_) + (-T34_)*(-T34_));
-        double nominator = (l1 * l1) + (l2 * l2) - d*d;
-        double denominator = 2*l1*l2;
-        //+-
-        theta4 = (PI - acos(nominator/denominator));
+        double d = sqrt(T_[0][3] * T_[0][3] + T_[1][3] * T_[1][3] + T_[2][3] * T_[2][3]);
+        double nominator = l1*l1 + l2*l2 - d*d;
+        double denom = 2*l1*l2;
+        theta4 = PI - acos(nominator/denom);
+        // TODO +- theta4
         return theta4;
     }
     
-    public double getTheta5()
+    double getTheta5()
     {
+        double nominator = T__[1][3]*(l2 + l1*cos(theta4)) + l1*T__[0][3]*sin(theta4);
+        double denominator = l1*l1*sin(theta4)*sin(theta4) + (l2 + l1*cos(theta4));
+        theta5 = asin(-nominator/denominator);
         return theta5;
+    }
+    
+    double getTheta5_2()
+    {
+        theta5 = PI - theta5;
+        return theta5;
+    }
+    
+    double getTheta6()
+    {
+        if(l2 * cos(theta5) + l1 * cos(theta4 + theta5) != 0)
+        {
+            theta6 = atan(T_[1][3] / T_[2][3]);
+        }
+        else
+        {
+            theta6 = 0;
+        }
+        return theta6;
+    }
+    
+    double getTheta2()
+    {
+        theta2 = acos(T___[1][2]);
+        // TODO +- theta2
+        // TODO - PI/4
+        return theta2;
+    }
+    
+    double getTheta3()
+    {
+        theta3 = asin(T___[1][1]/sin(theta2 + PI/4));
+        return theta3;
+    }
+    
+    double getTheta3_2()
+    {
+        theta3 = PI - theta3;
+        return theta3;
+    }
+    
+    double getTheta1()
+    {
+        theta1 = acos(T___[0][2]/sin(theta2 + PI/4));
+        // TODO +- theta1
+        // TODO + PI/2
+        return theta1;
+    }
+    
+    public Map<Joint, Double> getResult()
+    {
+        Map<Joint, Double> result = new HashMap<Joint, Double>();
+        theta4 = toDegrees(getTheta4());
+        if(Utils.validateJointRange(Joint.LLE4, theta4))
+        {
+            result.put(Joint.LLE4, theta4);
+        }
+        else if(Utils.validateJointRange(Joint.LLE4, -theta4))
+        {
+            theta4 = -theta4;
+            result.put(Joint.LLE4, theta4);
+        }
+        theta5 = toDegrees(getTheta5());
+        if(Utils.validateJointRange(Joint.LLE5, theta5))
+        {
+            result.put(Joint.LLE5, theta5);
+        }
+        else
+        {
+            theta5 = 180 - theta5;
+            if(Utils.validateJointRange(Joint.LLE5, theta5))
+            {
+                result.put(Joint.LLE5, theta5);
+            }
+        }
+        theta6 = toDegrees(getTheta6());
+        if(Utils.validateJointRange(Joint.LLE6, theta6))
+        {
+            result.put(Joint.LLE6, theta6);
+        }
+        
+        theta2 = toDegrees(getTheta2()) - 45;
+        if(Utils.validateJointRange(Joint.LLE2, theta2))
+        {
+            result.put(Joint.LLE2, theta2);
+        }
+        else 
+        {
+            theta2 = -toDegrees(getTheta2()) - 45;
+            if(Utils.validateJointRange(Joint.LLE2, theta2))
+            {
+                result.put(Joint.LLE2, theta2);
+            }
+        }
+        
+        theta3 = toDegrees(getTheta3());
+        if(Utils.validateJointRange(Joint.LLE3, theta3))
+        {
+            result.put(Joint.LLE3, theta3);
+        }
+        else 
+        {
+            theta3 = 180 - theta3;
+            if(Utils.validateJointRange(Joint.LLE3, theta3))
+            {
+                result.put(Joint.LLE3, theta3);
+            }
+        }
+        
+        theta1 = toDegrees(getTheta1()) + 90;
+        if(Utils.validateJointRange(Joint.LLE1, theta1))
+        {
+            result.put(Joint.LLE1, theta1);
+        }
+        else 
+        {
+            theta1 = -toDegrees(getTheta1()) + 90;
+            if(Utils.validateJointRange(Joint.LLE1, theta1))
+            {
+                result.put(Joint.LLE1, theta1);
+            }
+        }
+        return result;
     }
 }
