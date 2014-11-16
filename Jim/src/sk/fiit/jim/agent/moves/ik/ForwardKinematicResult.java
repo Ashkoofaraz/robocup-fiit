6f1c5dbacda79bd966852745a5d5cb3a0750c5fe
Paul P <paulp@hotmail.sk>
2014-11-16 14:55:56 +0100

    updated ForwardKinematicsResult

--- Jim/src/sk/fiit/jim/agent/moves/ik/ForwardKinematicResult.java
+++ Jim/src/sk/fiit/jim/agent/moves/ik/ForwardKinematicResult.java
@@ -16,14 +16,14 @@ public class ForwardKinematicResult
     
     private double az;
     
-    public ForwardKinematicResult(double[][] T)
+    public ForwardKinematicResult(Matrix T)
     {
-        px = T[0][3];
-        py = T[1][3];
-        pz = T[2][3];
-        ax = atan2(T[2][1], T[2][2]);
-        ay = atan2(-T[2][0], sqrt(T[2][1] * T[2][1] + T[2][2] * T[2][2]));
-        az = atan2(T[1][0], T[0][0]);
+        px = T.getValueAt(0, 3);
+        py = T.getValueAt(1, 3);
+        pz = T.getValueAt(2, 3);
+        ax = atan2(T.getValueAt(2, 1), T.getValueAt(2,2 ));
+        ay = atan2(-T.getValueAt(2, 0), sqrt(T.getValueAt(2, 1) * T.getValueAt(2, 1) + T.getValueAt(2, 2) * T.getValueAt(2, 2)));
+        az = atan2(T.getValueAt(1, 0), T.getValueAt(0, 0));
     }
 
     public double getPx()
