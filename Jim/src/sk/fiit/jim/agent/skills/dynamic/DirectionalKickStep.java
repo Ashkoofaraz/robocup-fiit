48ba0bca69f11684dfe233caadf31b767211064b
Paul P <paulp@hotmail.sk>
2015-05-03 11:06:42 +0200

    updated player shift equations

--- Jim/src/sk/fiit/jim/agent/skills/dynamic/DirectionalKickStep.java
+++ Jim/src/sk/fiit/jim/agent/skills/dynamic/DirectionalKickStep.java
@@ -54,11 +54,11 @@ public class DirectionalKickStep extends DynamicMove {
     private static void init(double alpha)
     {
         steps = 0;
-        double shift = calculateFootShift(alpha) - 0.055;
-        System.out.println("shift: " + shift);
-        double diff = shift - AgentModel.getInstance().getPosition().getY();
+        double playerShiftFromBall = calculatePlayerShiftLinear(alpha);
+        System.out.println("shift: " + playerShiftFromBall);
+        double diff = playerShiftFromBall - AgentModel.getInstance().getPosition().getY();
         maxSteps = (int) (Math.abs(diff)/DIST_STEP_VERY_SMALL);
-        if(shift > 0)
+        if(diff > 0)
         {
             step = "step_left_very_small";
         }
@@ -71,15 +71,11 @@ public class DirectionalKickStep extends DynamicMove {
     private static void init2(double alpha)
     {
         steps = 0;
-        double shift = calculateFootShift(alpha) - 0.055;
-        System.out.println("shift: " + shift);
-        double diff = shift - AgentModel.getInstance().getPosition().getY();
-        maxSmallSteps = (int) (Math.abs(diff)/DIST_STEP_SMALL);
-        double smallStepsDistance = maxSmallSteps * DIST_STEP_SMALL;
+        double playerShiftFromBall = calculatePlayerShiftLinear(alpha);
+        System.out.println("shift: " + playerShiftFromBall);
+        double diff = playerShiftFromBall - AgentModel.getInstance().getPosition().getY();
         
-        diff = diff- smallStepsDistance;
-        maxVerySmallSteps = (int) (Math.abs(diff)/DIST_STEP_VERY_SMALL);
-        if(shift > 0)
+        if(diff > 0)
         {
             verySmallStep = "step_left_very_small";
             smallStep = "step_left_small";
@@ -89,6 +85,13 @@ public class DirectionalKickStep extends DynamicMove {
             verySmallStep = "step_right_very_small";
             smallStep = "step_right_small";
         }
+        
+        maxSmallSteps = (int) (Math.abs(diff)/DIST_STEP_SMALL);
+        double smallStepsDistance = maxSmallSteps * DIST_STEP_SMALL;
+        
+        diff = diff- smallStepsDistance;
+        maxVerySmallSteps = (int) (Math.abs(diff)/DIST_STEP_VERY_SMALL);
+        
     }
     
 	@Override
@@ -127,11 +130,32 @@ public class DirectionalKickStep extends DynamicMove {
 	public void checkProgress() throws Exception {
 	}
 	
-    private static double calculateFootShift(double alpha)
+	private static double calculatePlayerShift(double alpha)
     {
         double shift = -2.798259731647e-7 * Math.pow(alpha, 4) + 1.48547703927838e-6 * Math.pow(alpha, 3)
-                + 0.0002393458 * Math.pow(alpha, 2) - 0.0037578029 * alpha - 0.0304023051;
+                + 0.0002393458 * Math.pow(alpha, 2) - 0.0037578029 * alpha - 0.0854023051;
         return shift;
+    }
+    
+    private static double calculatePlayerShiftLinear(double alpha)
+    {
+        if(alpha > 24.6)
+        {
+            return -0.11;
+        }
+        if(alpha < -25.9)
+        {
+            return 0.2;
+        }
+        if(alpha >= -25.9 && alpha <= 7)
+        {
+            return -0.0015539554 * alpha - 0.018402892;
+        }
+        if(alpha <= 24.6 && alpha > 7)
+        {
+            return -0.001020809*alpha - 0.0849254591;
+        }
+        return 0;
     }
 	
 	private static double getAlpha(Point3D point)
--- Jim/src/sk/fiit/jim/agent/skills/dynamic/DirectionalKickStepV2.java
+++ Jim/src/sk/fiit/jim/agent/skills/dynamic/DirectionalKickStepV2.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 
 import sk.fiit.jim.agent.models.AgentModel;
+import sk.fiit.jim.agent.models.WorldModel;
 import sk.fiit.jim.agent.moves.LowSkill;
 import sk.fiit.jim.agent.moves.LowSkills;
 import sk.fiit.jim.agent.moves.Phase;
@@ -15,116 +16,119 @@ public class DirectionalKickStepV2 extends DynamicMove {
     
     private static boolean kicked = false;
     
-    private double shift;
-    
     private double angle;
-    
-    private String side;
     
     public DirectionalKickStepV2(double angle)
     {
-        double agentActualY = AgentModel.getInstance().getPosition().getY();
-        System.out.println("Agent actual: " + agentActualY);
-        double agentRequiredY = calculateKickPosY(angle);
-        System.out.println("Agent requir: " + agentRequiredY);
         this.angle = angle;
-        this.shift = Math.abs(agentRequiredY - agentActualY);
-        if(agentRequiredY > agentActualY)
-        {
-            side = "left";
-        }
-        else
-        {
-            side = "right";
-        }
     }
     
     
 	@Override
 	public LowSkill pickLowSkill() {
-	    if(!stepped)
-	    {
-	        stepped = true;
-	        return createDynamicStep(side);
-	    }
-	    stepped = true;
-        if(stepped && !kicked) {
+	    double ballPosY = WorldModel.getInstance().getBall().getPosition().getY();
+        double agentPosY = AgentModel.getInstance().getPosition().getY();
+        double requiredShiftFromBall = calculatePlayerShiftLinear(angle);
+        double shiftDist = requiredShiftFromBall - agentPosY;
+        
+        System.out.println("step_shift: " + shiftDist );
+        
+        String step;
+        if(shiftDist > 0)
+        {
+            step = "step_left_very_small";
+        }
+        else
+        {
+            step = "step_right_very_small";
+        }
+        if(!stepped)
+        {
+            stepped = true;
+            return createDynamicStep(step, Math.abs(shiftDist));
+        }
+        if(!kicked)
+        {
             kicked = true;
             return LowSkills.get("kick_left_normal_stand");
         }
         return null;
 	}
 	
-	public LowSkill createDynamicStep(String side)
+	public LowSkill createDynamicStep(String step, double shift)
     {           
-        List<Phase> phases = getBaseSkillPhases(side);
+        List<Phase> phases = getPhasesForSkill(LowSkills.get(step));
         
         String ui = UUID.randomUUID().toString();
 
         ArrayList<String> types = new ArrayList<String>();
-        types.add("kick");   
+        types.add("step");   
         
         LowSkill ls = addSkill("dynamic_step_" + ui);
 
-        alterKickPhases(phases, side);
+        alterStepPhases(phases, step, shift);
             
         addPhases(phases, ls.name);
         return ls;
     }
 
-	private void alterKickPhases(List<Phase> phases, String side) 
-    {
-        if("left".equals(side))
-        {
-            phases.get(1).getEfectorData("RLE2").endAngle = calculateRLE2Angle(shift);
-        }
-        else
-        {
-            phases.get(1).getEfectorData("LLE2").endAngle = -calculateRLE2Angle(shift);
-        }
-    }
-	
-	private List<Phase> getBaseSkillPhases(String side)
-    {
-        LowSkill baseSkill = null;
-        if(side.equals("right"))
-        {
-            baseSkill = LowSkills.get("step_right_very_small");
-        }
-        else
-        {
-            baseSkill = LowSkills.get("step_left_very_small");
-        }
-        
-        System.out.println("BaseSkill: " + baseSkill);
-        return getPhasesForSkill(baseSkill);        
-    }
-	
 	@Override
 	public void checkProgress() throws Exception {
 	}
 	
-	private int calculateRLE2Angle(double shift)
-	{
-	    // TODO asi odstrnániť c, aby posun 0 bol aj uhol 0
-	    int angle = (int)(-334.2834552537 * shift - 3.9572967371);
-	    
-	    if(angle > 0)
-	    {
-	        angle = 0;
-	    }
-	    if(angle < -45)
-	    {
-	        angle = -45;
-	    }
-	    return angle; 
-	}
-	
-	 private static double calculateKickPosY(double alpha)
-	 {
-	     double shift = 2.28129772512726e-6 * Math.pow(alpha, 3)
-	             + 5.50804199512415e-5 * Math.pow(alpha, 2) - 0.0040088375 * alpha - 0.0746776193;
-	     return shift;
-	 }
+	private static double calculatePlayerShift(double alpha)
+    {
+        double shift = -2.798259731647e-7 * Math.pow(alpha, 4) + 1.48547703927838e-6 * Math.pow(alpha, 3)
+                + 0.0002393458 * Math.pow(alpha, 2) - 0.0037578029 * alpha - 0.0854023051;
+        return shift;
+    }
+    
+	private static double calculatePlayerShiftLinear(double alpha)
+    {
+        if(alpha > 24.6)
+        {
+            return -0.11;
+        }
+        if(alpha < -25.9)
+        {
+            return 0.2;
+        }
+        if(alpha >= -25.9 && alpha <= 7)
+        {
+            return -0.0015539554 * alpha - 0.018402892;
+        }
+        if(alpha <= 24.6 && alpha > 7)
+        {
+            return -0.001020809*alpha - 0.0849254591;
+        }
+        return 0;
+    }
+    
+    private int calculateLegAngle(double shift)
+    {
+        int angle = (int)(-334.2834552537 * shift - 3.9572967371);
 
+        if(angle > 0)
+        {
+            angle = 0;
+        }
+        if(angle < -45)
+        {
+            angle = -45;
+        }
+        return angle; 
+    }
+    
+    private void alterStepPhases(List<Phase> phases, String side, double shift) 
+    {
+        double shiftAngle = calculateLegAngle(shift);
+        if("step_left_very_small".equals(side))
+        {
+            phases.get(1).getEfectorData("RLE2").endAngle = shiftAngle;
+        }
+        else
+        {
+            phases.get(1).getEfectorData("LLE2").endAngle = -shiftAngle;
+        }
+    }
 }
